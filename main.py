d014f0f9adf64f0441e7f6fc9316b5d8a73487b8
Ryan Scopio <ryanscopio@gmail.com>
2021-04-29 18:58:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-#%% 
-# load mnist dataset
+#%% load mnist dataset
 from keras.datasets import mnist
+from tensorflow.keras import utils as np_utils
 from matplotlib import pyplot
-
-#%% 
-# preview data
 (trainX, trainY), (testX, testY) = mnist.load_data()
-print(f'Train: X={trainX.shape}, Y={trainY.shape}')
-print(f'Test: x={testX.shape}, y={testY.shape}')
 
-#%% 
-# plot first few images
+#%% preview data
 for i in range(9):
     pyplot.subplot(331 + i)
     pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
 pyplot.show()
+print(f'Train: X={trainX.shape}, Y={trainY.shape}')
+print(f'Test: x={testX.shape}, y={testY.shape}')
+
+#%% prepare data
+# reshape dataset to have a single channel
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+# one hot encode target values
+trainY = np_utils.to_categorical(trainY)
+testY = np_utils.to_categorical(testY)
+# preview reshaped data
+print(f'Train: X={trainX.shape}, Y={trainY.shape}')
+print(f'Test: x={testX.shape}, y={testY.shape}')
