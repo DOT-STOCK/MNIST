546bf63259079e63a81c218e90ec598593704cc8
Ryan Scopio <ryanscopio@gmail.com>
2021-04-29 17:56:29 -0700

    Jupyter Python Interactive Setup
    Loaded mnist database after realizing I had about 8 different python environments on my machine, lol

---
+++ main.py
@@ -0,0 +1,14 @@
+#%% load mnist dataset
+from keras.datasets import mnist
+from matplotlib import pyplot
+
+#%% preview data
+(trainX, trainY), (testX, testY) = mnist.load_data()
+print(f'Train: X={trainX.shape}, Y={trainY.shape}')
+print(f'Test: x={testX.shape}, y={testY.shape}')
+
+#%% plot first few images
+for i in range(9):
+    pyplot.subplot(331 + i)
+    pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
+pyplot.show()
