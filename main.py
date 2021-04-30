f111e642895792cd19ef0a39418ba4fba4f6bf2a
Ryan Scopio <ryanscopio@gmail.com>
2021-04-29 17:58:19 -0700

    Moved Comments

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-#%% load mnist dataset
+#%% 
+# load mnist dataset
 from keras.datasets import mnist
 from matplotlib import pyplot
 
-#%% preview data
+#%% 
+# preview data
 (trainX, trainY), (testX, testY) = mnist.load_data()
 print(f'Train: X={trainX.shape}, Y={trainY.shape}')
 print(f'Test: x={testX.shape}, y={testY.shape}')
 
-#%% plot first few images
+#%% 
+# plot first few images
 for i in range(9):
     pyplot.subplot(331 + i)
     pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
