5454f621f262933765174f186d4e8a7cd20e4d85
Ryan Scopio <ryanscopio@gmail.com>
2021-05-04 18:12:55 -0700

    Normalized values and defined CNN model

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #%% load mnist dataset
-from keras.datasets import mnist
-from tensorflow.keras import utils as np_utils
+from numpy import mean
+from numpy import std
 from matplotlib import pyplot
+from sklearn.model_selection import KFold
+from keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+#from keras_visualizer import visualizer
+
 (trainX, trainY), (testX, testY) = mnist.load_data()
 
 #%% preview data
@@ -17,8 +28,32 @@ print(f'Test: x={testX.shape}, y={testY.shape}')
 trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
 testX = testX.reshape((testX.shape[0], 28, 28, 1))
 # one hot encode target values
-trainY = np_utils.to_categorical(trainY)
-testY = np_utils.to_categorical(testY)
+trainY = to_categorical(trainY)
+testY = to_categorical(testY)
 # preview reshaped data
 print(f'Train: X={trainX.shape}, Y={trainY.shape}')
 print(f'Test: x={testX.shape}, y={testY.shape}')
+
+#%% normalize
+print(f'Center Slice Pre-Normalization:\n{trainX[0, 13:15, 13:15, 0]}')
+# convert from integers to floats
+trainX = trainX.astype('float32')
+testX = testX.astype('float32')
+#normalize to range 0-1
+trainX = trainX / 255.0
+testX = testX / 255.0
+print(f'\nCenter Slice Post-Normalization:\n{trainX[0, 13:15, 13:15, 0]}')
+
+#%% define cnn model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2,2)))
+model.add(Flatten())
+model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(10, activation='softmax'))
+# preview model
+model.summary()
+#visualizer(model, format='png', view=True)
+# compile model
+opt = SGD(learning_rate=0.01, momentum=0.9)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
